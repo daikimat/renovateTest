a013d6631f53c25f80e4fabfbb2548db21b06646
daikimat <daiki-matsumoto@cybozu.co.jp>
2021-06-01 10:02:41 +0900

    add empty project

---
+++ src/RenovateTest/RenovateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenovateTestApp.swift
+//  RenovateTest
+//
+//  Created by daiki-matsumoto on 2021/06/01.
+//
+
+import SwiftUI
+
+@main
+struct RenovateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
